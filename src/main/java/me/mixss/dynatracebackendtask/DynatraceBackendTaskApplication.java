9a7474328857a6775b36d93409e984823f64f872
Mixss <michalziemiec@wp.pl>
2023-04-21 14:36:15 +0200

    initial commit

---
+++ src/main/java/me/mixss/dynatracebackendtask/DynatraceBackendTaskApplication.java
@@ -0,0 +1,13 @@
+package me.mixss.dynatracebackendtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynatraceBackendTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynatraceBackendTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mixss/dynatracebackendtask/DynatraceBackendTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mixss.dynatracebackendtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynatraceBackendTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
